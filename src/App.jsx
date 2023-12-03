4df74ffba6f5b9587735aecc063f1260d4664a74
ganeshlonare <ganeshlonare311@gmail.com>
2023-12-03 12:55:11 +0530

    added header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <>
-      <h1 className='text-3xl'>Acme-Rockets</h1>
+      <Header />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-      
-    </div>
+      <header className='bg-teal-700 mb-1 sticky top-0'>
+        <div className='text-white flex p-4 justify-between items-center max-w-4xl mx-auto'>
+          <h1 className='text-3xl font-bold'>🚀Acma Rockets</h1>
+          <ul className='flex gap-10 text-xl'>
+            <li>Our Rockets</li>
+            <li>Testimonials</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </header>
   )
 }
