67285dcb569b4ad1be55c04513a353aebdb84ee0
ganeshlonare <ganeshlonare311@gmail.com>
2023-12-05 00:08:44 +0530

    creates section 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ export default function Header() {
         <div className='text-white flex p-4 justify-between items-center max-w-4xl mx-auto'>
           <h1 className='text-3xl font-bold'>🚀Acma Rockets</h1>
           <ul className='flex gap-10 text-xl'>
-            <li>Our Rockets</li>
-            <li>Testimonials</li>
-            <li>Contact Us</li>
+            <li className='hover:opacity-80 cursor-pointer'>Our Rockets</li>
+            <li className='hover:opacity-80 cursor-pointer'>Testimonials</li>
+            <li className='hover:opacity-80 cursor-pointer'>Contact Us</li>
           </ul>
         </div>
       </header>
--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function Rockets() {
   return (
-    <div>
-      
-    </div>
+      <div className='-mt-12 h-screen w-full text-white flex flex-col justify-center items-center max-w-4xl mx-auto'>
+      <h1 className='text-5xl mb-5 font-bold'>Our Rockets</h1>
+      <div className='flex'>
+      <div className='m-4 w-64 border h-64 rounded-3xl overflow-hidden flex flex-col justify-center items-center'>
+        <img className='h-32 mb-5' src="https://acme-dusky.vercel.app/img/rocketman.png" alt="not found" />
+        <h3 className='text-3xl'>Explorer</h3>
+        <h1 className='text-3xl text-slate-400'>$</h1>
+      </div>
+      <div className='m-4 w-64 border h-64 rounded-3xl overflow-hidden flex flex-col justify-center items-center'>
+        <img className='h-32 mb-5' src="https://acme-dusky.vercel.app/img/rocketride.png" alt="not found" />
+        <h3 className='text-3xl'>Adventure</h3>
+        <h1 className='text-3xl text-slate-400'>$$</h1>
+      </div>
+      <div className='m-4 w-64 border h-64 rounded-3xl overflow-hidden flex flex-col justify-center items-center'>
+        <img className='h-32 mb-5' src="https://acme-dusky.vercel.app/img/rocketlaunch.png" alt="not found" />
+        <h3 className='text-3xl'>Infinity</h3>
+        <h1 className='text-3xl text-slate-400'>$$</h1>
+      </div>
+      </div>
+      </div>
   )
 }
